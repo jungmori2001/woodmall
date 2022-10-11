5bd399658edb560038683a1c397e0f603479f48c
human <human@host.docker.internal>
2022-10-11 17:39:37 +0900

    20221011

---
+++ WoodMall/WebContent/login.jsp
@@ -0,0 +1,50 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/53a8c415f1.js"></script>
+    <link rel="stylesheet" href="css/login.css"> 
+    <script type="text/javascript" src="script/login.js"></script>
+<title>로그인</title>
+</head>
+<body>
+<body>
+<form action="login.do" method="post" name="frm">
+    <img src="image/logo.jpg">
+    <div class="wrap">
+        <div class="login">
+            <h2></h2>
+            <div class="login_sns">
+            <li><a href=""><i class="fab fa-instagram"></i></a></li>
+            <li><a href=""><i class="fab fa-facebook-f"></i></a></li>
+            <li><a href=""><i class="fab fa-twitter"></i></a></li>
+            </div>
+            <div class="login_id">
+                <h4>아이디</h4>
+                <input type="userid" name="userid" placeholder="아이디">
+            </div>
+            <div class="login_pw">
+                <h4>비밀번호</h4>
+                <input type="password" name="password"  placeholder="비밀번호">
+            </div>
+            <div class="login_etc">
+                <div class="checkbox">
+                <input type="checkbox" name="rembid" id=""> 아이디 저장
+                </div>
+                <div class="forgot_pw">
+                <a href="">비밀번호를 잃어버렸나요?</a>
+            </div>
+            </div>
+            <div class="submit">
+                <input type="submit" value="로그인" onclick="return checkLogin()"> 
+            </div>
+        </div>
+    </div>
+</form>
+</body>
+</html>
---
+++ WoodMall/src/main/java/com/woodmall/dto/MemberVo.java
@@ -0,0 +1,100 @@
+package com.woodmall.dto;
+
+public class MemberVo {
+	
+	/*
+	 * name VARCHAR(10), userid VARCHAR(10), password VARCHAR(20), emailId
+	 * VARCHAR(10), emailAddress VARCHAR(15), PhoneNum VARCHAR(5), postNum
+	 * VARCHAR(6), mainAddress VARCHAR(20), detailAddress VARCHAR(20), subAddress
+	 * VARCHAR(20), grade VARCHAR(20), admin NUMBER(1), totalPurchase NUMBER,
+	 */
+	
+	private String name;
+	private String userid;
+	private String password;
+	private String PhoneNum;
+	private String emailAddress;
+	private String postNum;
+	private String mainAddress;
+	private String detailAddress;
+	private String subAddress;
+	
+	
+	@Override
+	public String toString() {
+		return "MemberVO [name=" + name + ", userid=" + userid + ", password=" + password + ", PhoneNum=" + PhoneNum
+				+ ", emailAddress=" + emailAddress + ", postNum=" + postNum + ", mainAddress=" + mainAddress
+				+ ", detailAddress=" + detailAddress + ", subAddress=" + subAddress + "]";
+	}
+	
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhoneNum() {
+		return PhoneNum;
+	}
+	public void setPhoneNum(String phoneNum) {
+		PhoneNum = phoneNum;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getPostNum() {
+		return postNum;
+	}
+	public void setPostNum(String postNum) {
+		this.postNum = postNum;
+	}
+	public String getMainAddress() {
+		return mainAddress;
+	}
+	public void setMainAddress(String mainAddress) {
+		this.mainAddress = mainAddress;
+	}
+	public String getDetailAddress() {
+		return detailAddress;
+	}
+	public void setDetailAddress(String detailAddress) {
+		this.detailAddress = detailAddress;
+	}
+	public String getSubAddress() {
+		return subAddress;
+	}
+	
+	public void setSubAddress(String subAddress) {
+		this.subAddress = subAddress;
+	}
+
+
+
+
+
+	}
+	
+
+
+}
+
+
+
